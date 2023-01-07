bf0bbe516e853ed86ba335a1c65a0a4ef3bb4efa
wyatthaston <awhaston@protonmail.com>
2023-01-07 15:22:14 -0500

    Changed text to dummy text while in development

--- src/components/Sielox.svelte
+++ src/components/Sielox.svelte
@@ -1,18 +1,5 @@
 <section class="g-job">
-	<h2>Development-Operations Technician Intern</h2>
-	<h4>August 2022 - Present</h4>
-	<p>
-		Currently, while getting my bachelor's degree, I am a Dev-Ops Technician at Sielox LLC. Sielox
-		is a company that provides Access Control products to schools and companies with AnyWhere,
-		CLASS, and Pinnacle. I work with the Dev-Ops department to clean up source code and to bring
-		modern source control to the 2 decade old project Pinnacle.
-	</p>
-	<ul>
-		<li>Create YAML files to automate continuous development pipelines</li>
-		<li>Write windows batch scripts to copy source code to automate source control</li>
-		<li>Remotely connected to Windows 2003 to work with legacy source code</li>
-		<li>Create build scripts for Visual Build Pro</li>
-	</ul>
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 </section>
 
 <style>

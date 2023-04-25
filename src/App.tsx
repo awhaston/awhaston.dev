50ee62c457086c9618a75988acd2b45942cd2bf1
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2023-04-24 21:37:56 -0400

    Added Window

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
 import Taskbar from './components/Taskbar/Taskbar';
+import Window from './components/Taskbar/Window/Window';
 
 function App() {
     return (
         <div className="App">
-            <div>Test</div>
+            <Window></Window>
             <Taskbar></Taskbar>
         </div>
     );

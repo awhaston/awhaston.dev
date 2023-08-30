56a53b069536d6745505b112b04c176aaf9cbc16
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2023-08-29 20:54:30 -0500

    Added app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const PROJECTS = document.getElementById('projects');
+const PROJECTS_OVERFLOW = document.getElementById('projects-overflow');
+
+PROJECTS.addEventListener('click', (e) => {
+	PROJECTS_OVERFLOW.style.display = 'block';
+});

35db3e04dfe237e3e2dc9f3b056cd38d6e7876c9
Wyatt Haston <98433717+awhaston@users.noreply.github.com>
2023-08-29 23:45:18 -0500

    Removed app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const PROJECTS = document.getElementById('projects');
-const PROJECTS_OVERFLOW = document.getElementById('projects-overflow');
-
-PROJECTS.addEventListener('click', (e) => {
-	PROJECTS_OVERFLOW.style.display = 'block';
-});
